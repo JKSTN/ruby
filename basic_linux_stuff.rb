0883a3349b6b4af10cf14b4c8159fb9ae2c12271
JKSTN <44952323+JKSTN@users.noreply.github.com>
2018-11-15 21:21:09 +0000

    Add files via upload

---
+++ basic_linux_stuff.rb
@@ -0,0 +1,28 @@
+mkdir apps
+
+git clone https://github.com/JKSTN/ruby
+
+mv ruby apps
+
+cd Desktop/apps/ruby/1_basic_linux_stuff
+
+touch testing.rb 
+
+subl testing.rb
+
+ruby testing.rb 
+
+mkdir testing1
+
+touch testing1.rb 
+
+subl testing1.rb 
+
+cd 
+
+cd Desktop/apps/ruby 
+
+ruby 1_basic_linux_stuff/testing1.rb
+
+rm 1_basic_linux_stuff/testing1.rb
+rm -r 1_basic_linux_stuff/testing1
