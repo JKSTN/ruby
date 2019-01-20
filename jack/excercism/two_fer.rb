c6af38b6da79468f044d794e1d7e51b4a7de33bf
JKSTN <44952323+JKSTN@users.noreply.github.com>
2019-01-20 11:14:56 +0000

    Update two_fer.rb

--- jack/excercism/two_fer.rb
+++ jack/excercism/two_fer.rb
@@ -1,13 +1,5 @@
 class TwoFer
-	def self.no_name_given
-		"One for you, one for me."
-	end
-	def self.a_name_given
-		skip
-		"One for Alice, one for me."
-	end
-	def self.another_name_given
-		skip
-		"One for Bob, one for me."
-	end
+  def self.two_fer(name = "you")
+    "One for #{name}, one for me."
+  end
 end
