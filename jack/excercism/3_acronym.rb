d57f9a4aac30519806c05f404c69cbc265d13798
Hassan Abdul Rehman <hassan.rehman@gmail.com>
2019-01-23 10:10:30 +0000

    my version

--- jack/excercism/3_acronym.rb
+++ jack/excercism/3_acronym.rb
@@ -2,6 +2,13 @@ class Acronym
   def self.abbreviate(string = "Portable Network Graphic")
     string.scan(/(\A\w|(?<=\s)\w)|((?<=\-)\w)/).flatten.join.upcase
   end
+
+  def self.abbreviate_h(string = "Portable Network Graphic")
+    string.split(/\s+/)
+      .map{|s| s[0] }   #get first letters
+      .select{|s| /\A[A-Z]\z/ =~ s }    #only caps
+      .join
+  end
 end
 
 # regex or rubular is the code it means \A = start of the string
