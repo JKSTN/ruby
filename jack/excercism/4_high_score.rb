0c3ed304280154289dfc0862aee93efbdd58e841
Hassan Abdul Rehman <hassan.rehman@gmail.com>
2019-01-25 09:09:12 +0000

    conventions and alt solutions

--- jack/excercism/4_high_score.rb
+++ jack/excercism/4_high_score.rb
@@ -1,17 +1,40 @@
 class HighScoreTable
-	def self.HighScore(score)
-		array = score.sort!	{|a,b| b <=> a}
-		return array[0]
-	end
-	def self.Top3(score)
-		array = score.sort!	{|a,b| b <=> a}
-		return array[0], array[1], array[2]
-	end
-	def self.Range(score)
-		array = score.sort!	{|a,b| b <=> a}
-		return array[0] - array[-1]
-	end
-	def self.LastScore(score)
-		score[-1]
-	end
+
+  #HASSAN: convention dictates high_score ...
+  def self.HighScore(score)
+    #usually there's a good reason to use sort! since it changs the source
+    array = score.sort! {|a,b| b <=> a}
+
+    #HASSAN's alt 1:
+    # return score.sort.reverse[0]
+
+    #HASSAN's alt 2:
+    # return score.sort[-1]
+
+    #HASSAN's alt 2:
+    # return score.max
+
+    return array[0]
+  end
+
+  def self.Top3(score)
+    array = score.sort! {|a,b| b <=> a}
+
+    #alt
+    # array.sort.reverse[0..2]
+
+    #alt
+    # # array.sort.reverse.first(3)
+
+    return array[0], array[1], array[2]
+  end
+
+  #convention -- space after function end
+  def self.Range(score)
+    array = score.sort! {|a,b| b <=> a}
+    return array[0] - array[-1]
+  end
+  def self.LastScore(score)
+    score[-1]
+  end
 end
