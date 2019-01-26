2de7ca3c1da9a7d2a3ddda52870b2b87499796ff
JKSTN <44952323+JKSTN@users.noreply.github.com>
2019-01-26 10:20:33 +0000

    Update high_scores.rb

--- jack/excercism/high_scores.rb
+++ jack/excercism/high_scores.rb
@@ -1,29 +1,32 @@
 class HighScores
-	def initialize(scores)
-		@scores = scores
-	end
+  def initialize(scores)
+    @scores = scores
+  end
 
-	def scores
-		@scores
-	end
+  def scores
+    @scores
+  end
 
-	def personal_best
-		array = scores.sort!	{|a,b| b <=> a}
-		return array[0]
-	end
+  def personal_best
+    array = scores.sort!  {|a,b| b <=> a}
+    return array[0]
+  end
 
-	def personal_top
-		array = scores.sort!	{|a,b| b <=> a}
-		return array[0], array[1], array[2]
-	end
+  def personal_top
+    scores.sort.reverse.first(3)
+  end
 
-	def range
-		array = scores.sort!	{|a,b| b <=> a}
-		return array[0] - array[-1]
-	end
+  def range
+    array = scores.sort!  {|a,b| b <=> a}
+    return array[0] - array[-1]
+  end
 
-	def latest
-		scores[-1]
-	end
+  def latest
+    scores[-1]
+  end
+
+  def report
+    return "Your latest score was #{scores[-1]}. That's #{scores.max - scores[-1]} short of your personal best!"
+  end
 
 end
