dbe97d454ef201ff6230f957cb9404007c6fccc6
JKSTN <44952323+JKSTN@users.noreply.github.com>
2019-01-28 17:12:50 +0000

    Update 4_high_scores.rb

--- jack/excercism/main_exercises/4_high_scores.rb
+++ jack/excercism/main_exercises/4_high_scores.rb
@@ -3,32 +3,26 @@ class HighScores
     @scores = scores
   end
 
-  def scores
-    @scores
-  end
+  attr_reader :scores
 
   def personal_best
-    array = scores.sort!  {|a,b| b <=> a}
-    return array[0]
+    scores.max
   end
 
   def personal_top
     scores.sort.reverse.first(3)
   end
 
-  def range
-    array = scores.sort!  {|a,b| b <=> a}
-    return array[0] - array[-1]
-  end
-
   def latest
-    scores[-1]
+    scores.last
   end
 
   def report
-    #you can use 'latest' here ..  since it's part of the same object, you can call those methods
-    #like this, "your latest score was #{latest}. That's #{personal_best - latest} short of..."
-    return "Your latest score was #{scores[-1]}. That's #{scores.max - scores[-1]} short of your personal best!"
+    if personal_best - latest == 0
+      return "Your latest score was #{latest}. That's your personal best!"
+    else
+      "Your latest score was #{latest}. That's #{personal_best - latest} short of your personal best!"
+    end
   end
 
 end
