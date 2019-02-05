0aec886e2e98d1f0ecae4ae5e43297044b6ed024
JKSTN <44952323+JKSTN@users.noreply.github.com>
2019-02-05 11:38:11 +0000

    Update 5_matrix.rb

--- jack/excercism/main_exercises/5_matrix.rb
+++ jack/excercism/main_exercises/5_matrix.rb
@@ -7,14 +7,30 @@ class Matrix
   end
 
   def rows
-   matrix.split("\n").map do |row| 
+    matrix.split("\n").map do |row| 
       row.split(" ").map {|cell| cell.to_i }
     end
   end
 
-  def columns
-   matrix1 = matrix.split("\n").map{|row| row.split(" ").map{|cell| cell.to_i }  }
-   matrix2 = [ matrix1.map { |cell| cell[0] }, matrix1.map { |cell| cell[1] }, matrix1.map { |cell| cell[2] } ]
+  def columns_transpose
+    rows.transpose
+  end
+
+  def column_jack_original
+    matrix1 = matrix.split("\n").map{|row| row.split(" ").map{|cell| cell.to_i }  }
+    matrix2 = [ matrix1.map { |cell| cell[0] }, matrix1.map { |cell| cell[1] }, matrix1.map { |cell| cell[2] } ]
+  end
+
+  def column_transpose_manual
+    columns = rows[0].length.times.map{ [] }
+    rows.each_with_index do |row, r|
+      row.each_with_index do |cell, c|
+        puts "#{r}, #{c} / print #{cell}"
+        columns[c][r] = cell
+      end
+    end
+    # puts columns.map { |column| column.inspect }
+    return columns
   end
 
  end
