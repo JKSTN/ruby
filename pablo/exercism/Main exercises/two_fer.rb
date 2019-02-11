fc069e8ee138516a3d5587993bd3b8370c766700
psguemes <30631967+psguemes@users.noreply.github.com>
2019-02-11 17:57:46 +0000

    Add files via upload

---
+++ pablo/exercism/Main exercises/two_fer.rb
@@ -0,0 +1,17 @@
+class TwoFer
+    def two_fer(name = "you")        
+       "One for #{name}, one for me."        
+    end
+end
+
+puts TwoFer.two_fer
+
+
+
+#{}"One for X, one for me."
+#{}```
+
+#When X is a name or "you".
+
+#If the given name is "Alice", the result should be "One for Alice, one for me."
+#If no name is given, the result should be "One for you, one for me."
