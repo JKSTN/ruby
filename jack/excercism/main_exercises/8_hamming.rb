c4c40f6c6fb631874eb1fef6a2817b7007b589e7
JKSTN <44952323+JKSTN@users.noreply.github.com>
2019-02-21 17:27:56 +0000

    Update 8_hamming.rb

--- jack/excercism/main_exercises/8_hamming.rb
+++ jack/excercism/main_exercises/8_hamming.rb
@@ -1,6 +1,6 @@
 class Hamming
 
-  def self.compute(string1, string2)
+  def self.compute(string1, string2)_jack
     if string1.length == string2.length
       elements_boolean = string1.split("").zip(string2.split("")).map { |x, y| x == y }
       elements_boolean.count(false)
@@ -8,7 +8,21 @@ class Hamming
       raise ArgumentError
     end
 
-  end
+  def self.compute(string1, string2)_hassan
+    if string1.length == string2.length
+      string1.length.times.count{|i| string1[i] == string2[i]   }
+    else
+      raise ArgumentError
+    end  
+
+  # string1.length.times.count{|i| string1[i] == string2[i]   }
+  # i = 0
+  # count = 0
+  # loop do 
+  #   break if string1[i].nil?
+  #   count += 1 if string1[i] == string2[i]
+  #   i += 1
+  # end
+
 
 end
-
